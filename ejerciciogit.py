568d9d6d21275965ebf82da83f600a463e4354d9
Juan Gabriel Serrano Anaya <juangserrano2@gmail.com>
2024-11-30 15:28:50 -0500

    Se agregan más variables a la suma

--- ejerciciogit.py
+++ ejerciciogit.py
@@ -1,4 +1,7 @@
-Juan Gabriel Serrano Anaya
+#Juan Gabriel Serrano Anaya
 # Este es un comentario
-print("Hola Mundo")
-numero2 = int(print("Escriba un NUMERO: "))
+print("Suma de números")
+numero1 = int(print("Escriba el primer número: "))
+numero2 = int(print("Escriba el segundo número: "))
+resultado = numero1 + numero2
+print("La suma de ",numero1," + ",numero2," es: ", resultado)
